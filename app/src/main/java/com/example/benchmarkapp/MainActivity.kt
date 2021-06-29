cc04a1405ab4e729ca299b434def2c9900c29133
yoshiki0425 <judging5@gmail.com>
2021-06-29 16:36:33 +0900

    ROM取得UIのレイアウトのみ設置

--- app/src/main/java/com/example/benchmarkapp/MainActivity.kt
+++ app/src/main/java/com/example/benchmarkapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.benchmarkapp
 
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
-import android.os.Handler
+import android.os.*
+import android.os.storage.StorageManager
+import android.os.storage.StorageVolume
 import android.provider.Settings
 import android.util.Log
 import android.widget.Button
@@ -167,6 +167,68 @@ class MainActivity : AppCompatActivity() {
                 })
             }
         }, 1, 1000) //1ミリ秒後にintervalミリ秒ごとの繰り返し
+    }
+
+    @SuppressLint("DiscouragedPrivateApi")
+    private fun getRomUsage(){
+
+        val storageManager = getSystemService(StorageManager::class.java)
+
+        //システムストレージの取得
+        val statFs_system = StatFs("/system")
+        // 総容量
+        val totalSpase1: Long = statFs_system.blockCountLong * statFs_system.blockSizeLong / 1024L / 1024L
+        // 空き容量
+        val freeSpase1: Long = statFs_system.availableBlocksLong * statFs_system.blockSizeLong / 1024L / 1024L
+        // 使用容量
+        val usedSpase1: Long = totalSpase1 - freeSpase1
+        Log.d(TAG,"Label:システムストレージ")
+        Log.d(TAG, " Used space: ${String.format(Locale.US, "%,12d", usedSpase1)}MB")
+        Log.d(TAG, " Free space: ${String.format(Locale.US, "%,12d", freeSpase1)}MB")
+        Log.d(TAG, "Total space: ${String.format(Locale.US, "%,12d", totalSpase1)}MB")
+
+
+        for (storageVolume: StorageVolume in storageManager.storageVolumes) {
+            // ストレージボリュームのリストを取得し、1件づつ処理
+            // ストレージボリュームから絶対パスを取得
+            val path: String = when {
+                Build.VERSION.SDK_INT>= Build.VERSION_CODES.R -> {
+                    // Android 11以降
+                    storageVolume.directory?.absolutePath
+                }
+                else -> {
+                    // Android 10以前
+                    // NOTE: Android10以前ではgetPathがprivateなため無理やり実行して取得
+                    val getPath = StorageVolume::class.java.getDeclaredMethod("getPath")
+                    getPath.invoke(storageVolume) as String?
+                }
+            } ?: continue // 絶対パスが取得できない場合は、スキップ
+
+            val label: String = when {
+                Build.VERSION.SDK_INT>= Build.VERSION_CODES.R -> {
+                    // Android 11以降
+                    storageVolume.getDescription(applicationContext)
+                }
+                else -> {
+                    storageVolume.getDescription(applicationContext)
+                }
+            } ?: continue // 絶対パスが取得できない場合は、スキップ
+
+            val statFs = StatFs(path)
+            // 総容量
+            val totalSpase: Long = statFs.blockCountLong * statFs.blockSizeLong / 1024L / 1024L
+            // 空き容量
+            val freeSpase: Long = statFs.availableBlocksLong * statFs.blockSizeLong / 1024L / 1024L
+            // 使用容量
+            val usedSpase: Long = totalSpase - freeSpase
+
+            Log.d(TAG, "Path: $path")
+            Log.d(TAG,"Label: $label")
+            Log.d(TAG, " Used space: ${String.format(Locale.US, "%,12d", usedSpase)}MB")
+            Log.d(TAG, " Free space: ${String.format(Locale.US, "%,12d", freeSpase)}MB")
+            Log.d(TAG, "Total space: ${String.format(Locale.US, "%,12d", totalSpase)}MB")
+        }
+
     }
 
 
